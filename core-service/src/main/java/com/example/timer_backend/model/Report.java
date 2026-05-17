d308451e3cc056d8b67156523ca81c71adff3737
literandltx <romanskovronn@gmail.com>
2026-05-17 23:51:43 +0300

    fix: checkstyle cleanup

--- core-service/src/main/java/com/example/timer_backend/model/Report.java
+++ core-service/src/main/java/com/example/timer_backend/model/Report.java
@@ -1,7 +1,13 @@
 package com.example.timer_backend.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
