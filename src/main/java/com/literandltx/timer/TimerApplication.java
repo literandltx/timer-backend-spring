0dce83618b740df29823173432b19f66d657ff5f
literandltx <romanskovronn@gmail.com>
2026-06-12 15:41:23 +0300

    chore: update entry point

--- src/main/java/com/literandltx/timer/TimerApplication.java
+++ src/main/java/com/literandltx/timer/TimerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TimerBackendSpringApplication {
+public class TimerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TimerBackendSpringApplication.class, args);
+        SpringApplication.run(TimerApplication.class, args);
     }
 
 }
