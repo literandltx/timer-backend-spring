dfc6488abde89ffc6c4ad190a15fd398bc713d2b
literandltx <romanskovronn@gmail.com>
2026-06-12 14:48:38 +0300

    docs: add ownership validation java docs

--- src/main/java/com/literandltx/timer/model/UserOwned.java
+++ src/main/java/com/literandltx/timer/model/UserOwned.java
@@ -2,10 +2,22 @@ package com.literandltx.timer.model;
 
 import java.util.UUID;
 
+/**
+ * Represents an entity that is owned by a specific user.
+ * Implementing this interface allows for standardized ownership validation.
+ */
 public interface UserOwned {
 
-    User getUser();
-
+    /**
+     * Retrieves the unique identifier of the entity.
+     * * @return the UUID of the entity
+     */
     UUID getUuid();
+
+    /**
+     * Retrieves the user who owns this entity.
+     * * @return the owning User
+     */
+    User getUser();
 
 }
