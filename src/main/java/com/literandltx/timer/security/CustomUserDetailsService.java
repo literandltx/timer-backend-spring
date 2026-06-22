54f415aece85358f0968e1b80b43807e0aada1fb
literandltx <romanskovronn@gmail.com>
2026-06-22 23:46:38 +0300

    feat: optimize auth process

--- src/main/java/com/literandltx/timer/security/CustomUserDetailsService.java
+++ src/main/java/com/literandltx/timer/security/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.literandltx.timer.security;
 
 import com.literandltx.timer.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,8 +14,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email).orElseThrow(
+    public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
+        return userRepository.findByEmailForAuth(email).orElseThrow(
                 () -> new UsernameNotFoundException("Cannot find user by email"));
     }
 }
