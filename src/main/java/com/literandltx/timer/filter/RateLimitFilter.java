f35133b9681c8b5bf718ba375eaeab13dfb33d3f
literandltx <romanskovronn@gmail.com>
2026-06-29 23:36:14 +0300

    feat: adjustments

--- src/main/java/com/literandltx/timer/filter/RateLimitFilter.java
+++ src/main/java/com/literandltx/timer/filter/RateLimitFilter.java
@@ -6,11 +6,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class RateLimitFilter extends OncePerRequestFilter {
 
     private final RateLimiter rateLimiter;
--- src/main/java/com/literandltx/timer/service/RateLimiter.java
+++ src/main/java/com/literandltx/timer/service/RateLimiter.java
@@ -1,5 +1,8 @@
 package com.literandltx.timer.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface RateLimiter {
 
     /**
