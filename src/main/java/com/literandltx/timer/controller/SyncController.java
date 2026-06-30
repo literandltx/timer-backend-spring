088029166b19d8e9dfb045d79317a175caee88af
literandltx <romanskovronn@gmail.com>
2026-06-30 16:47:31 +0300

    feat: small adjustments

--- src/main/java/com/literandltx/timer/controller/SyncController.java
+++ src/main/java/com/literandltx/timer/controller/SyncController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/sync")
 public class SyncController {
 
-    private SyncService syncService;
+    private final SyncService syncService;
 
     @PostMapping("/queue")
     public ResponseEntity<SyncQueueBulkResponse> processQueue(
